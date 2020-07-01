279928960bb353b89f8d41b2df3a747dcff0766b
Edward-K1 <eddyk1collab@gmail.com>
2020-07-01 15:29:42 +0300

    initial commit

---
+++ luxinapi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for luxinapi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luxinapi.settings')
+
+application = get_asgi_application()
