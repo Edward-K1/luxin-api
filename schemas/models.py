bc7b202c77a6304ec1a9fa3c0800350f10b6e3a1
Edward-K1 <eddyk1collab@gmail.com>
2020-07-11 23:41:29 +0300

    fetch questions from assessment instance

--- schemas/models.py
+++ schemas/models.py
@@ -11,6 +11,18 @@ class LuxinUser(User):
 class Assessment(models.Model):
     name = models.CharField(max_length=100)
 
+    @property
+    def questions(self):
+        questions = Question.objects.filter(assessment=self)
+        question_and_options = []
+        for qn in questions:
+            options = [x.text for x in Answer.objects.filter(question=qn)]
+            question_and_options.append({
+                'Question': qn.text,
+                'Options': options
+            })
+        return question_and_options
+
 
 class Question(models.Model):
     assessment = models.ForeignKey(Assessment, on_delete=models.CASCADE)
